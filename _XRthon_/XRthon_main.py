2219488a68c1ff39698cabf85d7b799730eebbda
是星星与然然呀（XR） <XR>
2024-02-10 17:54:42 +0800

    XRthon_front_file 0.1 BETA __0.0.1__ V

---
+++ _XRthon_/XRthon_main.py
@@ -0,0 +1,204 @@
+import os, pathlib, time
+
+folder = pathlib.Path(__file__).parent.resolve()
+
+class XRthon_main():
+    def __init__(self):
+        self.var_ = {}
+
+    def raisess(self, text, types, code_=1):
+        print(f'{types}: Error: {text}, Status code: {code_}')
+
+    def XRthon_text(self, text_code):
+        texts = ''.join(text_code)
+        list_1 = texts.split('\n')
+        for text in list_1:
+            if '#' in text:
+                continue
+            elif '=' in text:
+                list_ = text.split('=')
+                name = list_[0]
+                v = list_[1]
+                if 'input' in v:
+                    v_list_ = v.split(':')
+                    text_2 = list_[1].split('\'')
+                    for text_1 in text_2:
+                        if  text_1 == '__name__' or text_1 ==  '__path__' or text_1 == '__init__' or text_1 == '__file__' or text_1 == '__package__':
+                            time.sleep(0.5)
+                            self.raisess('Variable cannot be printed', 'Variable cannot be printed')
+                        if not text_1 in self.var_:
+                            time.sleep(0.5)
+                            self.raisess('Variable cannot be printed', 'Variable cannot be printed')
+                        else:
+                            print(self.var_[text_1], end='')
+                        if text_1 in self.var_:
+                            print(self.var_[text_1], end='')
+                        else:
+                            if not text_1 == '':
+                                print(v_list_[1], end='')
+                        v = input('')
+                elif '{}' in v:
+                    vs = set()
+                    v_list_ = v.split('{}')
+                    for vars in v_list_:
+                        vars_list_ = vars.split(',')
+                        for vars_ in vars_list_:
+                            vs.update({vars_})
+                            self.var_.update({name : vs})
+                self.var_.update({name : v})
+                time.sleep(0.5)
+                print('A variable has been created')
+            elif ':' in text:
+                list_ = text.split(':')
+                f = list_[0]
+                if f == 'print':
+                    text_2 = list_[1].split('\'')
+                    for text_1 in text_2:
+                        if  text_1 == '__name__' or text_1 == '__path__' or text_1 == '__init__' or text_1 == '__file__' or text_1 == '__package__':
+                            time.sleep(0.5)
+                            self.raisess('Variable cannot be printed', 'Variable cannot be printed')
+                        if not text_1 in self.var_:
+                            time.sleep(0.5)
+                            self.raisess('Variable cannot be printed', 'Variable cannot be printed')
+                        else:
+                            print(self.var_[text_1])
+                        if text_1 in self.var_:
+                            print(self.var_[text_1])
+                        else:
+                            if not text_1 == '':
+                                print(list_[1])
+                elif f == 'input':
+                    text_2 = list_[1].split('\'')
+                    for text_1 in text_2:
+                        if  text_1 == '__name__' or text_1 ==  '__path__' or text_1 == '__init__' or text_1 == '__file__' or text_1 == '__package__':
+                            time.sleep(0.5)
+                            self.raisess('Variable cannot be printed', 'Variable cannot be printed')
+                        if not text_1 in self.var_:
+                            time.sleep(0.5)
+                            self.raisess('Variable cannot be printed', 'Variable cannot be printed')
+                        else:
+                            print(self.var_[text_1], end='')
+                        if text_1 in self.var_:
+                            print(self.var_[text_1], end='')
+                        else:
+                            if not text_1 == '':
+                                print(self.var_[1], end='')
+                elif f == 'init':
+                    time.sleep(0.5)
+                    self.raisess('Failed to initialize', 'Failed to initialize')
+            else:
+                time.sleep(0.5)
+                self.raisess(f'unknown code, code: {text}', 'unknown code')
+
+    def XRthon_file(self, file, path=os.path.join(folder, './test_files/TEST.XRn')):
+        var = {}
+        run_codes_ = []
+        texts = ''.join(file.read())
+        file_now_line = 0
+        list_1 = texts.split('\n')
+        for text in list_1:
+            code_isE = False
+            file_now_line += 1
+            if file_now_line == 1:
+                if 'init' in text:
+                    list_ = text.split(':')
+                    if '#' in list_[0]:
+                        time.sleep(0.5)
+                        self.raisess('\'init\'undefined', '\'init\'undefined')
+                        continue
+                    paths_ = ''.join(path)
+                    path_ = paths_.split('\\')
+                    path_len_ = len(path_) - 1
+                    pathss_ = path_[path_len_].split('.')
+                    pathss_len_ = len(pathss_) - 1
+                    var.update({'__name__' : pathss_[pathss_len_ -1], '__path__' : path, '__init__' : list_[1] , '__file__' : path_[path_len_], '__package__' : path_[path_len_ - 1]})
+                    continue
+                else:
+                    time.sleep(0.5)
+                    self.raisess('\'init\'undefined', '\'init\'undefined')
+                    continue
+            if '#' in text:
+                continue
+            elif '=' in text:
+                list_ = text.split('=')
+                v_name_ = list_[0]
+                v = list_[1]
+                if 'input' in v:
+                    v_list_ = v.split(':')
+                    if v_list_[0] == 'input':
+                        text_2 = v_list_[1].split('\'')
+                        for text_1 in text_2:
+                            if text_1 in var:
+                                print(var[text_1], end='')
+                            else:
+                                if not text_1 == '':
+                                    print(v_list_[1], end='')
+                            v = input('')
+                elif '{' in v and '}' in v:
+                    vs = set( )
+                    v_list_ = v.split('{')[1].split('}')
+                    for vars in v_list_:
+                        if vars == '':
+                            continue
+                        vars_list_ = vars.split(',')
+                        for vars_ in vars_list_:
+                            vs.update({vars_})
+                            var.update({v_name_ : vs})
+                var.update({v_name_ : v})
+            elif ':' in text:
+                list_ = text.split(':')
+                f = list_[0]
+                if f == 'print':
+                    text_2 = list_[1].split('\'')
+                    for text_1 in text_2:
+                        if  text_1 == '__name__' or text_1 == '__path__' or text_1 == '__init__' or text_1 == '__file__' or text_1 == '__package__':
+                            if not text_1 in var:
+                                time.sleep(0.5)
+                                self.raisess('\'init\' undefined, special variable cannot be printed', '\'init\' undefined')
+                            else:
+                                print(var[text_1])
+                        elif text_1 in var:
+                            print(var[text_1])
+                        else:
+                            if not text_1 == '':
+                                print(list_[1])
+                elif f == 'input':
+                    text_2 = list_[1].split('\'')
+                    for text_1 in text_2:
+                        if  text_1 == '__name__' or text_1 ==  '__path__' or text_1 == '__init__' or text_1 == '__file__' or text_1 == '__package__':
+                            if not text_1 in var:
+                                time.sleep(0.5)
+                                self.raisess('\'init\' undefined, special variable cannot be printed', '\'init\' undefined')
+                            else:
+                                print(var[text_1], end='')
+                        elif text_1 in var:
+                            print(var[text_1], end='')
+                        else:
+                            if not text_1 == '':
+                                print(list_[1], end='')
+                        input('')
+                elif f == 'return':
+                    print('The program has exited')
+                    return
+                elif f == 'raise':
+                    text_2 = list_[1].split(',')
+                    if len(text_2) == 2:
+                        self.raisess(text_2[0], text_2[1])
+                        return
+                    if len(text_2) == 3:
+                        self.raisess(text_2[0], text[1], int(text_2[2]))
+                        return
+                else:
+                    time.sleep(0.5)
+                    code_isE = True
+                    self.raisess(f'Unknown code, code: {text}', 'unknown code')
+            else:
+                time.sleep(0.5)
+                code_isE = True
+                self.raisess(f'Unknown code, code: {text}', 'unknown code')
+            if code_isE == True:
+                pass
+            elif code_isE == False:
+                run_codes_.append(text)
+
+_XRthon_main_ = XRthon_main()
---
+++ _XRthon_/main.py
@@ -0,0 +1,64 @@
+import os, pathlib, time, shutil
+from . import XRthon_main
+
+def mains():
+    folder = pathlib.Path(__file__).parent.resolve()
+
+    var_ = {}
+
+    pycache = os.path.join(folder, './__pycache__')
+    if os.path.exists(pycache):
+        shutil.rmtree(pycache)
+
+    print('''
+    ##################
+    #                #
+    #  XRthon 1.0.0  #
+    #                #
+    ##################
+    ''')
+
+    print('Now edition: 0.1 BETA __0.0.1__')
+    print('Note that the \'BETA\' in the version number is the major version number 0 (beta version)')
+    print('For example, \'XRthon 0.1 BETA __0.0.1__ V\' means that the XRthon version is \'0.0.1_0.0.1\'')
+
+    time.sleep(0.5)
+
+    def Main_logic():
+
+        running = True
+
+        while running:
+            command = input('Enter the command (enter "q" to exit, "R_F" run "XRn" file, "T_C" literal code(the interpreter writes the code)) >>>')
+            time.sleep(0.5)
+            if command == 'q':
+                running = False
+                return
+            elif command == 'R_F':
+                path = input('Enter the path ("XRn" file type) >>>')
+                time.sleep(0.5)
+                paths = path.split('.')
+                if not paths[len(paths) - 1] == 'XRn':
+                    time.sleep(0.5)
+                    print('Wrong file type (should be "XRn"), or an error has occurred')
+                    running = False
+                    Main_logic()
+                    return
+                try:
+                    with open(os.path.join(path), 'r', encoding='UTF-8') as file:
+                        XRthon_main._XRthon_main_.XRthon_file(file, path=path)
+                except:
+                    print('An error has occurred')
+            elif command == 'T_C':
+                text_code = input('Enter the code >>>')
+                time.sleep(0.5)
+                try:
+                    XRthon_main._XRthon_main_.XRthon_text(text_code)
+                except:
+                    time.sleep(0.5)
+                    print('An error has occurred')
+            else:
+                time.sleep(0.5)
+                print(f'No command \"{command}\"')
+                
+    Main_logic()
